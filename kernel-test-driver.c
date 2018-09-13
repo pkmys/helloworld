db8273c2e334f87d62bc804fedf4f009cd57ba23
Pawan Kumar <jmppawanhit@gmail.com>
2018-09-13 21:50:35 +0530

    added ioctl feature. BUG: not working proper getting wrong value

--- kernel-test-driver.c
+++ kernel-test-driver.c
@@ -17,49 +17,57 @@
 #include <linux/module.h>       // required for all
 #include <linux/kernel.h>       // for kernel loglevels like KERN_INFO
 #include <linux/init.h>	        // nedded if we want to use user defined name for init and exit
-#include <linux/device.h>       // Header to support the kernel Driver Model
+//#include <linux/device.h>     // Header to support the kernel Driver Model [optional]
 #include <linux/fs.h>           // Header for the Linux file system support
 #include <linux/uaccess.h>      // Required for the copy to user function
-#include <linux/string.h>
+#include <linux/ioctl.h>		// For ioctl support
 
+
+
+#define DEVICE_NAME "testdev"   // The device will appear at /dev/testdev using this message
+#define CLASS_NAME  "test"      // The device class -- this is a character device driver
+#define MOD_AUTHOR "Pawan Kumar <jmppawanhit@gmail.com>"
+#define MOD_DESC "Hello world driver"
+#define MOD_SUPPORT "test device"
+#define MAJOR_NUMBER 240
 
 /*******************************************
  *                modinfo                  *
  *******************************************/
-#define  DEVICE_NAME "testdev"   // The device will appear at /dev/testdev using this value
-#define  CLASS_NAME  "test"      // The device class -- this is a character device driver
-#define MOD_AUTHOR "Pawan Kumar <jmppawanhit@gmail.com>"
-#define MOD_DESC "Hello world driver"
-#define MOD_SUPPORT "test device"
-
-MODULE_LICENSE("Dual BSD/GPL");         // for getting rid of kernel taint warning
+MODULE_LICENSE("Dual BSD/GPL");         // open source licensing
 MODULE_AUTHOR(MOD_AUTHOR);              // name of the module author
 MODULE_DESCRIPTION(MOD_DESC);           // a short description about module
-MODULE_VERSION("0.0.1");                // A version number to inform users
+MODULE_VERSION("0.1");                  // A version number to inform users
 MODULE_SUPPORTED_DEVICE(MOD_SUPPORT);   // devices this module uses
 
 
 /*******************************************
  *                 global                  *
  *******************************************/
-static int    majorNumber = 241;                  // Stores the device number -- determined automatically
-static char   message[1024];           // Memory for the string that is passed from userspace
-static short  size_of_message;              // Used to remember the size of the string stored
-static int    numberOpens = 0;              // Counts the number of times the device is opened
-//static struct class*  testdevClass  = NULL; // The device-driver class struct pointer
-//static struct device* testdevDevice = NULL; // The device-driver device struct pointer
+
+static char   message[1023];                // Memory for the string that is passed from userspace
+static short  sizeOfMessage;                // Used to remember the size of the string stored
+static int    lock;                         // Counts the number of times the device is opened
+static int32_t mode;
+#define WR_VALUE _IOW(MAJOR_NUMBER, 0,int32_t*)     // #define "IOCTL_NAME" __IOX("magic number","command number","argument type")
+#define RD_VALUE _IOR(MAJOR_NUMBER, 1,int32_t*)     // refer: https://embetronicx.com/tutorials/linux/device-drivers/ioctl-tutorial-in-linux/
+/*  optional for creating the device file  
+static struct class*  testdevClass  = NULL; // The device-driver class struct pointer
+static struct device* testdevDevice = NULL; // The device-driver device struct pointer
+*/
 
 
 /********************************************
  *                prototypes                *
  ********************************************/
-static int testdev_init(void);
-static void testdev_exit(void);
+static int     testdev_init(void);
+static void    testdev_exit(void);
 // The prototype functions for the character driver -- must come before the struct definition
 static int     dev_open(struct inode *, struct file *);
-static int     dev_release(struct inode *, struct file *);
 static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t dev_write(struct file *, const char *, size_t, loff_t *);
+static long    dev_ioctl(struct file *, unsigned int , unsigned long );
+static int     dev_release(struct inode *, struct file *);
 
 
 /* @brief Devices are represented as file structure in the kernel. The file_operations structure from
@@ -72,6 +80,7 @@ static struct file_operations fops =
    .open = dev_open,
    .read = dev_read,
    .write = dev_write,
+   .unlocked_ioctl = dev_ioctl,
    .release = dev_release,
 };
 
@@ -86,20 +95,21 @@ static int __init testdev_init(void)
 {
 	int8_t ret;
 	printk(KERN_INFO "%d %s: Initializing test driver...\n",__LINE__,__func__);
-	// Try to dynamically allocate a major number for the device 
-    	ret = register_chrdev(majorNumber, DEVICE_NAME, &fops);   
+	// allocate the device major number for testdev 
+    	ret = register_chrdev(MAJOR_NUMBER, DEVICE_NAME, &fops);   
 	if (ret<0){
     		printk(KERN_ALERT "%d %s: testdev: failed to register a major number\n",__LINE__,__func__);
 		
 		return ret;
 	}
 
-	printk(KERN_INFO "%d %s: testdev: registered correctly with major number %d\n",__LINE__, __func__, majorNumber);
+	printk(KERN_INFO "%d %s: testdev: registered correctly with major number %d\n",__LINE__, __func__, MAJOR_NUMBER);
+
 /*
    	// Register the device class
    	testdevClass = class_create(THIS_MODULE, CLASS_NAME);
    	if (IS_ERR(testdevClass)){                // Check for error and clean up if there is
-    		unregister_chrdev(majorNumber, DEVICE_NAME);
+    		unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);
         	printk(KERN_ALERT "%d %s: testdev: failed to register device class\n",__LINE__, __func__);
 	
 		return PTR_ERR(testdevClass);     // Correct way to return an error on a pointer
@@ -108,10 +118,10 @@ static int __init testdev_init(void)
    	printk(KERN_INFO "%d %s: testdev: device class registered correctly\n",__LINE__, __func__);
 
    	// Register the device driver
-   	testdevDevice = device_create(testdevClass, NULL, MKDEV(majorNumber, 0), NULL, DEVICE_NAME);
+   	testdevDevice = device_create(testdevClass, NULL, MKDEV(MAJOR_NUMBER, 0), NULL, DEVICE_NAME);
    	if (IS_ERR(testdevDevice)){                    // Clean up if there is an error
         	class_destroy(testdevClass);           // Repeated code but the alternative is goto statements
-        	unregister_chrdev(majorNumber, DEVICE_NAME);
+        	unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);
         	printk(KERN_ALERT "%d %s: testdev: Failed to create the device\n",__LINE__, __func__);
       
 	    	return PTR_ERR(testdevDevice);
@@ -128,25 +138,25 @@ static int __init testdev_init(void)
  */
 static void __exit testdev_exit(void)
 {
-	//device_destroy(testdevClass, MKDEV(majorNumber, 0));     // remove the device
-   	//class_unregister(testdevClass);                          // unregister the device class
-   	//class_destroy(testdevClass);                             // remove the device class
-   	unregister_chrdev(majorNumber, DEVICE_NAME);               // unregister the major number
+	//device_destroy(testdevClass, MKDEV(MAJOR_NUMBER, 0));     // remove the device
+   	//class_unregister(testdevClass);                           // unregister the device class
+   	//class_destroy(testdevClass);                              // remove the device class
+   	unregister_chrdev(MAJOR_NUMBER, DEVICE_NAME);               // unregister the major number
 	
 	printk(KERN_INFO "%d %s: testdev: driver deregistrartion success\n",__LINE__,__func__);
 }
 
 
 /* @brief The device open function that is called each time the device is opened
- * This will only increment the numberOpens counter in this case.
+ * This will only increment the lock counter in this case.
  * @param inodep A pointer to an inode object (defined in linux/fs.h)
  * @param filep A pointer to a file object (defined in linux/fs.h)
  */
 static int dev_open(struct inode *inodep, struct file *filep){
 
-   	numberOpens++;
+   	lock++;
 
-   	printk(KERN_INFO "%d %s: testdev: device has been opened %d time(s)\n",__LINE__,__func__, numberOpens);
+   	printk(KERN_INFO "%d %s: testdev: device has been opened %d time(s)\n",__LINE__,__func__, lock);
 
    	return 0;
 }
@@ -163,12 +173,11 @@ static int dev_open(struct inode *inodep, struct file *filep){
 static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *offset){
    	int error_count = 0;
 	   
-	printk("temp is %s size is %d", message, size_of_message);
-	error_count = copy_to_user(buffer, &message, size_of_message);
+	error_count = copy_to_user(buffer, &message, sizeOfMessage);
 
    	if (error_count==0){                 // if true then have success
-      		printk(KERN_INFO "%d %s: testdev: sent %d characters to the user\n",__LINE__,__func__, size_of_message);
-			size_of_message=0;    // clear the position to the start and return 0  
+      		printk(KERN_INFO "%d %s: testdev: sent %d characters to the user\n",__LINE__,__func__, sizeOfMessage);
+			sizeOfMessage=0;    // clear the position to the start and return 0  
    	}else {
 
       		printk(KERN_INFO "%d %s: testdev: failed to send %d characters to the user\n",__LINE__,__func__, error_count);
@@ -176,13 +185,13 @@ static ssize_t dev_read(struct file *filep, char *buffer, size_t len, loff_t *of
 	return -EFAULT;                      // Failed -- return a bad address message (i.e. -14)
    	}
 
-return size_of_message;
+return sizeOfMessage;
 }
 
 
 /* @brief This function is called whenever the device is being written to from user space i.e.
  * data is sent to the device from the user. The data is copied to the message[] array in this
- * LKM using the sprintf() function along with the length of the string.
+ * LKM using the copy_from_user() function along with the length of the string.
  * @param filep A pointer to a file object
  * @param buffer The buffer to that contains the string to write to the device
  * @param len The length of the array of data that is being passed in the const char buffer
@@ -190,11 +199,32 @@ return size_of_message;
  */
 static ssize_t dev_write(struct file *filep, const char *buffer, size_t len, loff_t *offset){
 
-	size_of_message = len;
+	sizeOfMessage = len;
 	copy_from_user(message, buffer, len);
    	printk(KERN_INFO "%d %s: testdev: received %zu characters from the user\n",__LINE__,__func__, len);
    
 	return len;
+}
+
+
+/* @brief This function is called whenever the ioctl function is invoked from user space i.e.
+ * user will call ioctl with some mode and argument.
+ * @param file <file descriptor>: This the open file on which the ioctl command needs to be executed, which would generally be device files.
+ * @param cmd <ioctl command>: ioctl command which is implemented to achieve the desired functionality
+ * @param arg <arguments>: The arguments that needs to be passed to the ioctl command.
+ */
+static long dev_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+         switch(cmd) {
+                case WR_VALUE:
+                        copy_from_user(&mode , (int32_t*)arg, sizeof(mode));
+                        printk(KERN_INFO "%d %s: ioctl_op: value = %d\n", __LINE__,__func__, mode);
+                        break;
+                case RD_VALUE:
+                        copy_to_user((int32_t*)arg, &mode, sizeof(mode));
+                        break;
+        }
+        return 0;
 }
 
 
--- user-test-driver.c
+++ user-test-driver.c
@@ -12,50 +12,95 @@
 /******************************************
  *               includes                 *
  ******************************************/
-#include<stdio.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<fcntl.h>
-#include<string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
 #include<unistd.h>
- 
-#define BUFFER_LENGTH 256               // The buffer length (crude but fine)
+#include<fcntl.h>
+
+#define BUFFER_LENGTH 256               // The buffer length (crude but fine) 
+#define WR_VALUE _IOW('a','a',int32_t*)
+#define RD_VALUE _IOR('a','b',int32_t*)
+
 static char receive[BUFFER_LENGTH];     // The receive buffer from the LKM
+static char stringToSend[BUFFER_LENGTH];
+
+int testrdwr(int fd)
+{
+    int ret;
+
+    printf("Type in a short string to send to the kernel module:\n");
+    fflush(stdin);
+    scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
+    printf("Writing message to the device [%s].\n", stringToSend);
+
+    ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
+    if (ret < 0){
+        perror("Failed to write the message to the device.");
+        return errno;
+    }
+
+    printf("Press ENTER to read back from the device...\n");
+    getchar();
+
+    printf("Reading from the device...\n");
+    ret = read(fd, receive, BUFFER_LENGTH);       // Read the response from the LKM
+    if (ret < 0){
+        perror("Failed to read the message from the device.");
+        return errno;
+    }
+
+    printf("The received message is: [%s]\n", receive);
+    return 0;
+}
  
 int main(){
-   int ret, fd;
-   char stringToSend[BUFFER_LENGTH];
 
-   printf("Starting device test code example...\n");
+   int fd;
+   int choice;
+   int32_t value, number;
 
-   fd = open("testdev", O_RDWR);             // Open the device with read/write access
+   fd = open("testdev", O_RDWR);        // Open the device with read/write access
    if (fd < 0){
       perror("Failed to open the device...");
       return errno;
    }
+start:
+   printf("*****************************************\n");
+   printf("*                test menu              *\n");
+   printf("*****************************************\n");
+   printf("1. Test device read-write\n");
+   printf("2. Test ioctl operation\n");
+   printf("Enter your choice\n");
+  
+   scanf("%d",&choice);
 
-   printf("Type in a short string to send to the kernel module:\n");
-   scanf("%[^\n]%*c", stringToSend);                // Read in a string (with spaces)
-   printf("Writing message to the device [%s].\n", stringToSend);
-
-   ret = write(fd, stringToSend, strlen(stringToSend)); // Send the string to the LKM
-   if (ret < 0){
-      perror("Failed to write the message to the device.");
-      return errno;
-   }
+    switch (choice) {
+    case 1: {
+        testrdwr(fd);
+        break;
+    }
+    case 2: {
+        printf("Enter the Value to send\n");
+        scanf("%d",&number);
+        printf("Writing Value to Driver\n");
+        ioctl(fd, WR_VALUE, (int32_t*)&number); 
  
-   printf("Press ENTER to read back from the device...\n");
-   getchar();
- 
-   printf("Reading from the device...\n");
-   ret = read(fd, receive, BUFFER_LENGTH);       // Read the response from the LKM
-   if (ret < 0){
-      perror("Failed to read the message from the device.");
-      return errno;
-   }
-
-   printf("The received message is: [%s]\n", receive);
-   printf("End of the program\n");
+        printf("Reading Value from Driver\n");
+        ioctl(fd, RD_VALUE, (int32_t*)&value);
+        printf("Value is %d\n", value);     
+        break;
+    }
+    default:
+        printf("Wrong input\n");
+        goto start;
+    }
    
+   printf("End of the program\n");
+   close(fd);
    return 0;
 }
